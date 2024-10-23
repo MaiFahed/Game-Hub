ea8e7b21d0be163accffb9d650209b1c7b08fee3
MaiFahed <maifahed16@gmail.com>
2024-10-23 18:04:17 +0300

    Cleaning up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,8 @@
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenre";
 import { GameQuery } from "../App";
 
 interface Props{
@@ -20,8 +19,8 @@ const GameGrid = ({gameQuery}: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={4}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={5}
         padding="10px"
       >
         {isLoading &&
