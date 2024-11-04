fd53adf571cad3fbbbadfd79d767b9a7eee5a2f3
MaiFahed <maifahed16@gmail.com>
2024-11-04 17:33:20 +0200

    Build game Attributes

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,20 +1,21 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import useDetailGame from "../hooks/useDetailGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
-
+import GameAttributes from "../components/GameAttributes";
 const GameDetailPage = () => {
-    const { slug } = useParams();
-    const {data: game, isLoading, error} = useDetailGame(slug!); // ! means that the slug will never be null
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useDetailGame(slug!); // ! means that the slug will never be null
 
-    if(isLoading) return <Spinner />;
-    if(error || !game) throw error;
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
   return (
     <>
-    <Heading>{game.name}</Heading>
-    <ExpandableText>{game.description_raw}</ExpandableText>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game}/>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
