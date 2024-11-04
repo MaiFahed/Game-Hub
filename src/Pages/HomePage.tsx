ca041d04ac4f37f9355c32f761210037ed288694
MaiFahed <maifahed16@gmail.com>
2024-11-04 15:17:44 +0200

    setup routing

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,33 +1,29 @@
 import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import GameGrid from "../components/GameGrid";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+import GameHeading from "../components/GameHeading";
 
-function App() {
+const HomePage = () => {
   // for filtering: instead of adding a punch of filters then add state to every element we can wrap all the filters(element) in one object query known by query object pattern.
   //  const [selectedGenre , setSelectedGenre] = useState<Genre | null>(null);
   //  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
-  
+
   // now we dont have to use query object, instead we used zustand to manage the state of the app
   // const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -45,6 +41,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
